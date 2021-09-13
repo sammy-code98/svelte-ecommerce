af29c658da924cc92216424d6e793ab9410b906b
sammy-code98 <sammysmart98@yahoo.com>
2021-09-13 15:19:53 +0100

    Feat:add navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,56 @@
 <script>
-	export let name;
+	import Search from "./Search.svelte"
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <nav class="nav">
+		<span class="nav-log">Ecommerce</span>
+		<Search class="search"/>
+      <ul>
+        <li><a href="#">Account</a></li>
+        <li><a href="#">Cart</a></li>
+        <li><a href="#">Help</a></li>
+      </ul>
+    </nav>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	
+  .nav {
+    display: flex;
+	justify-content: space-between;
+	align-self: center;
+	background-color: #fff;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
+	width: 100%;
+  }
+  .nav-log{
+	  font-size: 30px;
+	  font-weight: 500;
+	  color:#282828 ;
+	  margin-left: 12px;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .nav ul {
+    list-style-type: none;
+    margin: 0;
+	padding-top: 0;
+    padding-left: 10px;
+	float: right;
+  }
+  .nav ul li{
+	  float: left;
+  }
+  .nav ul li a{
+	  display: block;
+	  text-decoration: none;
+	  padding: 14px 16px;
+	  color:#282828;
+  }
+  .nav ul li a:hover{
+	  color: #f68b1e;;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  }
 </style>
+
+
---
+++ src/Search.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<main>
+  <form action="#">
+    <input type="text" placeholder="Search anythine here ....."/>
+    <input type="submit" value="Search" />
+  </form>
+</main>
+
+<style>
+    form{
+        margin-top: 5px;
+    }
+    input[type=text]{
+        border-radius: 8px;
+        padding: 10px;
+
+    }
+    input[type=submit]{
+        border: none;
+        border-radius: 8px;
+        background-color: #f68b1e;
+        color: #fff;
+        padding: 10px;
+        margin-right: 12px;
+    }
+</style>
