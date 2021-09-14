49883b4b04845aed35d8c0a57fdebc86be0d4264
sammy-code98 <sammysmart98@yahoo.com>
2021-09-14 23:44:59 +0100

    update

--- src/Othersell.svelte
+++ src/Othersell.svelte
@@ -5,36 +5,115 @@
     <div class="text">Walk Shop Categories</div>
     <div class="items">
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/80/953127/1.jpg?2370" alt="jum" class="sell-image">
-        <p class="title">Men Sneakers</p>
-        <p class="title">#5000</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/1-ws-mens-sneakers_260x144.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Men's Sneakers</p>
       </div>
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/72/245656/1.jpg?0874" alt="jum" class="sell-image">
-        <p class="title">Andriod Phone</p>
-        <p class="title">#52,000</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/2-ws-womens-sneakers_260x144.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Women's Sneakers</p>
+
       </div>
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/68/815536/1.jpg?9587" alt="jum" class="sell-image">
-        <p class="title">Laptop Bag</p>
-        <p class="title">#5400</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/3-ws-kids-sneakers_260x144.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Kid's Sneakers</p>
+
       </div>
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/23/245656/1.jpg?7851" alt="jum" class="sell-image">
-        <p class="title">Oppo Reno</p>
-        <p class="title">#35000</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/14-ws-womens-boots_260x144.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Mens Sneakers</p>
+
       </div>
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/22/370822/1.jpg?5639" alt="jum" class="sell-image">
-        <p class="title">Men Polo</p>
-        <p class="title">#8000</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/4-ws-mens-slippers_260x144.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Men's Slippers</p>
+
       </div>
       <div class="sell-card">
-        <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/30/550396/1.jpg?8783" alt="jum" class="sell-image">
-        <p class="title">Earpods</p>
-        <p class="title">#5000</p>
+        <img
+          src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/5-ws-women-sandals.png"
+          alt="jum"
+          class="sell-image"
+        />
+        <p class="title">Women's Sandals</p>
+
       </div>
     </div>
+    <div class="items">
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/6-ws-mens-formal-shoes_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+          <p class="title">Men's Formal</p>
+
+        </div>
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/7-ws-womens-heels_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+        </div>
+        <p class="title">Women's Heels</p>
+
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/11-ws-loafers_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+          <p class="title">Men's Loafers</p>
+
+        </div>
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/12-ws-flats_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+          <p class="title">Women's Flats</p>
+
+        </div>
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/13-ws-mens-boots_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+          <p class="title">Men's Boots</p>
+
+        </div>
+        <div class="sell-card">
+          <img
+            src="https://ng.jumia.is/cms/0-1-weekly-cps/2021/w36/categories/walk-shop/14-ws-womens-boots_260x144.png"
+            alt="jum"
+            class="sell-image"
+          />
+          <p class="title">Women's Boots</p>
+
+        </div>
+      </div>
   </div>
 </main>
 
@@ -45,11 +124,10 @@
     background-color: #fff;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     margin: auto;
- 
   }
   .text {
-      background-color: #FEE2CC;
-      text-align: center;
+    background-color: #fee2cc;
+    text-align: center;
     padding: 12px;
     font: 35px;
     font-weight: 500;
@@ -59,29 +137,27 @@
     flex-direction: row;
     justify-content: space-between;
     margin-right: 20px;
-    padding: 20px;
+    padding: 10px;
   }
-  .sell-card{
+  .sell-card {
     height: 100%;
     width: 200px;
     margin: 10px;
     border-radius: 8px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     cursor: pointer;
-    transition: transform .2s;
+    transition: transform 0.2s;
   }
-  .sell-image{
+  .sell-image {
     width: 100%;
     border-top-right-radius: 8px;
     border-top-left-radius: 8px;
-    
-    
   }
-  .title{
-    font: 20px;
-    margin-left: 12px;
+  .title {
+    font: 10px;
+    text-align: center;
   }
-  .sell-card:hover{
-    transform: scale(0.95); 
-     }
+  .sell-card:hover {
+    transform: scale(0.95);
+  }
 </style>
