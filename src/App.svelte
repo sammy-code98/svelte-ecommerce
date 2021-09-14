c47068ad6c5ca06a6a6e1934b2069e183da54a06
sammy-code98 <sammysmart98@yahoo.com>
2021-09-14 11:56:15 +0100

    Fix:nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,76 @@
 <script>
-	import Search from "./Search.svelte"
+  // import Search from "./Search.svelte";
+  import Navbar from  "./Nav.svelte"
 </script>
 
 <main>
-    <nav class="nav">
-		<span class="nav-log">Ecommerce</span>
-		<Search/>
-      <ul>
-        <li><a href="#">Account</a></li>
-        <li><a href="#">Cart</a></li>
-        <li><a href="#">Help</a></li>
-      </ul>
-    </nav>
+  <Navbar />
+  <!-- <nav class="nav">
+    <span class="nav-log">Ecommerce</span>
+    <Search />
+    <ul>
+      <li><a href="#">Account</a></li>
+      <li><a href="#">Cart</a></li>
+      <li><a href="#">Help</a></li>
+    </ul>
+  </nav> -->
+
 </main>
 
 <style>
-	
-  .nav {
+  /* .nav {
     display: flex;
-	justify-content: space-between;
-	align-self: center;
-	background-color: #fff;
-	box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-	width: 100%;
+    justify-content: space-between;
+    align-self: center;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
+    width: 100%;
   }
-  .nav-log{
-	  font-size: 30px;
-	  font-weight: 500;
-	  color:#282828 ;
-	  margin-left: 12px;
+  .nav-log {
+    font-size: 30px;
+    font-weight: 500;
+    color: #282828;
+    margin-left: 12px;
   }
 
   .nav ul {
     list-style-type: none;
     margin: 0;
-	padding-top: 0;
+    padding-top: 0;
     padding-left: 10px;
-	float: right;
+    float: right;
   }
-  .nav ul li{
-	  float: left;
+  .nav ul li {
+    float: left;
   }
-  .nav ul li a{
-	  display: block;
-	  text-decoration: none;
-	  padding: 14px 16px;
-	  color:#282828;
+  .nav ul li a {
+    display: block;
+    text-decoration: none;
+    padding: 14px 16px;
+    color: #282828;
   }
-  .nav ul li a:hover{
-	  color: #f68b1e;;
+  .nav ul li a:hover {
+    color: #f68b1e;
+  }
+  .harmburgar {
+    display: none;
+  }
+  .bar {
+    display: block;
+    width: 25px;
+    height: 3px;
+    margin: 5px auto;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    background-color: #f68b1e;
+  }
 
-  }
+  @media only screen and (min-width: 668px) {
+    .nav ul {
+      position: fixed;
+      left: -100%;
+      top: 5rem;
+      flex-direction: column;
+    } */
+  /* } */
 </style>
-
-
---
+++ src/Nav.svelte
@@ -0,0 +1,59 @@
+<script>
+    import Search from "./Search.svelte"
+</script>
+
+<main>
+  <nav class="navbar">
+    <span class="nav-logo">Ecommerce</span>
+    <Search />
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Account</a>
+
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Help</a>
+            
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Cart</a>
+            
+        </li>
+    </ul>
+  </nav>
+</main>
+
+<style>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    padding:1rem;
+    height: 20px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
+
+  }
+  .nav-logo {
+    font-size: 30px;
+    font-weight: 500;
+    color: #f68b1e;
+  }
+  .nav-menu{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+  }
+  .nav-item{
+      margin-left: 3rem;
+      list-style: none;
+  }
+  .nav-link{
+      font-size: 19px;
+      color: #282828;
+      text-decoration: none;
+  }
+  .nav-link:hover{
+      color:#f68b1e;
+  }
+</style>
