ef5fc5b85e882ca668c89c73011817f50402f17b
sammy-code98 <sammysmart98@yahoo.com>
2021-09-25 00:04:14 +0100

    updated and clean up

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,10 +1,17 @@
-<script></script>
+<script>
+   import Icon from 'svelte-awesome';
+   import { search } from 'svelte-awesome/icons';
+
+</script>
 
 <main>
   <form action="#">
     <input type="text" placeholder="Search anythine here ....."/>
     <input type="submit" value="Search" />
   </form>
+  <div class="mobile-search">
+<Icon  data={search} scale="1.5" style="border: none; border-radius: 8px; padding: 2px; color:#f68b1e; margin-top:8px"/>
+  </div>
 </main>
 
 <style>
@@ -27,9 +34,15 @@
     input[type=text]:focus{
       outline: none;
     }
+    .mobile-search{
+      display: none;
+    }
     @media only screen and (max-width: 668px) {
       form{
         display: none;
+      }
+      .mobile-search{
+        display: block;
       }
     }
 </style>
