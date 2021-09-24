87fc9a2efcc9f416b7cdc384101e23479a7fa39b
sammy-code98 <sammysmart98@yahoo.com>
2021-09-24 23:27:07 +0100

    fix:responsive footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,7 @@
     <span class="nav-logo">Ecommerce</span>
     <div class="newsletter">
       <p class="text">New to Ecommerce ?</p>
-      <span class="text2">Subscribe to our newsletter</span>
+      <p class="text2">Subscribe to our newsletter</p>
       <form action="#">
         <input type="email" placeholder="Enter E-mail Address" />
         <input type="submit" value="Male" />
@@ -13,19 +13,19 @@
       </form>
     </div>
     <div>
-        <p class="text tex">DOWNLOAD ECOMMERCE FREE APP</p>
-        <span class="text2">Get access to exclusive offers</span>
-        <br />
-        <br />
-        <button>App store</button>
-        <button>Google play</button>
+      <p class="text tex">DOWNLOAD ECOMMERCE FREE APP</p>
+      <p class="text2">Get access to exclusive offers</p>
+      <br />
+      <br />
+      <button>App store</button>
+      <button>Google play</button>
     </div>
   </div>
 </main>
 
 <style>
   .footer {
-      width: 100%; 
+    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -43,35 +43,63 @@
   .text {
     color: #fff;
   }
-  .tex{
-      margin-right: 12px;
+  .tex {
+    margin-right: 12px;
   }
   .text2 {
     color: #fff;
     font-size: 15px;
   }
-  form{
-      margin-top: 12px;
+  form {
+    margin-top: 12px;
   }
-  input[type="email"]{
-      border: none;
-      border-radius: 4px;
-      padding: 10px;
+  input[type="email"] {
+    border: none;
+    border-radius: 4px;
+    padding: 10px;
   }
-  input[type ="submit"]{
-      border: none;
-      border-radius: 4px;
-      padding: 10px;
-      margin: 4px;
-      color: #f68b1e;
-      cursor: pointer;
+  input[type="submit"] {
+    border: none;
+    border-radius: 4px;
+    padding: 10px;
+    margin: 4px;
+    color: #f68b1e;
+    cursor: pointer;
   }
-  button{
-      padding: 10px;
-      border: none;
-      border-radius: 4px;
-      color: #f68b1e;
-      cursor: pointer;
-      margin: 15px;
+  button {
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    color: #f68b1e;
+    cursor: pointer;
+    margin: 15px;
+  }
+  @media only screen and (max-width: 768px) {
+    .footer {
+      display: block;
+    }
+    .nav-logo {
+      text-align: center;
+    }
+    .text {
+      text-align: center;
+    }
+    .text2 {
+      text-align: center;
+    }
+    form {
+      display: inline;
+    }
+    input[type="email"] {
+      width: 90%;
+      margin: 20px;
+    }
+    input[type="submit"] {
+      margin-left: 70px;
+    }
+    button {
+      margin-left: 60px;
+
+    }
   }
 </style>
